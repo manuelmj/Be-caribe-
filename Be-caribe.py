18bb6f0a3c537f08d9daaac8c3309604b47342a1
Luis Alejandro <luisalejandronietoruth@gmail.com>
2022-06-07 19:29:11 -0500

    Implementacion del blog V1

--- Be-caribe.py
+++ Be-caribe.py
@@ -15,18 +15,19 @@ moment = Moment(app)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 
+
 class DateForm(FlaskForm):
-    date_from = DateField('Datos desde:',validators=[DataRequired()])
-    date_to = DateField('hasta:',validators=[DataRequired()])
+    date_from = DateField('Datos desde:', validators=[DataRequired()])
+    date_to = DateField('hasta:', validators=[DataRequired()])
     submit = SubmitField('Buscar')
 
 
 def _data_gauge(device_name):
-    
+
     variables = devices_var(device_name)
     json_data = json.dumps({'temperature': float(variables.rt_temperature),
                             'humidity': float(variables.rt_humidity)})
-    
+
     yield f"data:{json_data}\n\n"
 
 
@@ -37,23 +38,31 @@ def index():
     return render_template('index.html', devices=devices)
 
 
+@app.route('/blog')
+def blog():
+    devices = devices_info()
+
+    return render_template('blog.html', devices=devices)
+
+
 @app.route('/dashboard/<device_name>', methods=['GET', 'POST'])
 def dashboard(device_name):
     form = DateForm()
     if form.validate_on_submit():
-        data = devices_graph(device_name,   
-                            datetime.combine(form.date_from.data, time()),
-                            datetime.combine(form.date_to.data, time()))
+        data = devices_graph(device_name,
+                             datetime.combine(form.date_from.data, time()),
+                             datetime.combine(form.date_to.data, time()))
 
         return render_template('dashboard.html', device_name=device_name, data=data, form=form)
-    
-    return render_template('dashboard.html', device_name=device_name, 
+
+    return render_template('dashboard.html', device_name=device_name,
                            form=form, data=[])
+
 
 @app.route('/tiempo_real/<device_name>')
 def flujo_tiempo_real(device_name):
     enviar = _data_gauge(device_name)
-    
+
     return Response(stream_with_context(enviar), mimetype='text/event-stream')
 
 
